44758f53be9e71eb9402da305ae44a621216c1c7
kiarasht15 <90223700+kiarasht15@users.noreply.github.com>
2021-12-05 22:25:27 -0500

    Update Football.py

--- Football.py
+++ Football.py
@@ -1,12 +1,12 @@
 import random
 teams = ["Arizona Cardinals", "Atlanta Falcons", "Carolina Panthers", "Dallas Cowboys", "Chicago Bears", "Detroit Lions", "Green Bay Packers", "New York Giants", "Los Angeles Rams", "Minnesota Vikings", "New Orleans Saints", "Philadelphia Eagles", "San Francisco 49ers", "Seattle Seahawks", "Tampa Bay Buccaneers", "Washington Football Team"]
-players = ["Baltimore Ravens", "Buffalo Bills", "Cincinatti Bengals", "Cleveland Browns", "Denver Broncos", "Houston Texans", "Indianapolis Colts", "Jacksonville Jaguars", "Kansas City Chiefs", "Las Vegas Raiders", "Los Angeles Chargers", "Miami Dolphins", "New England Patriots", "New York Jets", "Pittsburgh Steelers", "Tennessee Titans"]
+contestants = ["Baltimore Ravens", "Buffalo Bills", "Cincinatti Bengals", "Cleveland Browns", "Denver Broncos", "Houston Texans", "Indianapolis Colts", "Jacksonville Jaguars", "Kansas City Chiefs", "Las Vegas Raiders", "Los Angeles Chargers", "Miami Dolphins", "New England Patriots", "New York Jets", "Pittsburgh Steelers", "Tennessee Titans"]
 
 class Game:
     def __init__(self, name):
         self.name = name
 
-class Player(Game):
+class Contestant(Game):
     def get_team(self):
         teams = dict()
         random.shuffle(self.name)
@@ -14,7 +14,7 @@ class Player(Game):
         teams['AFC'] = self.name[1]
         return teams
 
-my_team = Player(players)
+my_team = Contestant(contestants)
 my_team = my_team.get_team()
 for team, name in my_team.items():
     print(team, name) 
