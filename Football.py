a9468aff639d9a63a6ec815b53282c79e27a365e
kiarasht15 <90223700+kiarasht15@users.noreply.github.com>
2021-12-05 22:23:11 -0500

    Update Football.py

---
+++ Football.py
@@ -0,0 +1,20 @@
+import random
+teams = ["Arizona Cardinals", "Atlanta Falcons", "Carolina Panthers", "Dallas Cowboys", "Chicago Bears", "Detroit Lions", "Green Bay Packers", "New York Giants", "Los Angeles Rams", "Minnesota Vikings", "New Orleans Saints", "Philadelphia Eagles", "San Francisco 49ers", "Seattle Seahawks", "Tampa Bay Buccaneers", "Washington Football Team"]
+players = ["Baltimore Ravens", "Buffalo Bills", "Cincinatti Bengals", "Cleveland Browns", "Denver Broncos", "Houston Texans", "Indianapolis Colts", "Jacksonville Jaguars", "Kansas City Chiefs", "Las Vegas Raiders", "Los Angeles Chargers", "Miami Dolphins", "New England Patriots", "New York Jets", "Pittsburgh Steelers", "Tennessee Titans"]
+
+class Game:
+    def __init__(self, name):
+        self.name = name
+
+class Player(Game):
+    def get_team(self):
+        teams = dict()
+        random.shuffle(self.name)
+        teams['NFC'] = self.name[0]
+        teams['AFC'] = self.name[1]
+        return teams
+
+my_team = Player(players)
+my_team = my_team.get_team()
+for team, name in my_team.items():
+    print(team, name) 
