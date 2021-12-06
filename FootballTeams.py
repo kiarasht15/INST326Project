22834db46e067609b7031002fe7cc83bd88df4fb
kiarasht15 <90223700+kiarasht15@users.noreply.github.com>
2021-12-05 22:12:32 -0500

    Update FootballTeams.py

--- FootballTeams.py
+++ FootballTeams.py
@@ -1,28 +1,20 @@
 import random
-import sys
+teams = ["Arizona Cardinals", "Atlanta Falcons", "Carolina Panthers", "Dallas Cowboys", "Chicago Bears", "Detroit Lions", "Green Bay Packers", "New York Giants", "Los Angeles Rams", "Minnesota Vikings", "New Orleans Saints", "Philadelphia Eagles", "San Francisco 49ers", "Seattle Seahawks", "Tampa Bay Buccaneers", "Washington Football Team"]
+players = ["Baltimore Ravens", "Buffalo Bills", "Cincinatti Bengals", "Cleveland Browns", "Denver Broncos", "Houston Texans", "Indianapolis Colts", "Jacksonville Jaguars", "Kansas City Chiefs", "Las Vegas Raiders", "Los Angeles Chargers", "Miami Dolphins", "New England Patriots", "New York Jets", "Pittsburgh Steelers", "Tennessee Titans"]
 
-class footballGame: 
-    nfc_team = ""
-    afc_team = ""
+class Game:
+    def __init__(self, name):
+        self.name = name
 
-    def nfcTeamRandom():
-        nfc_team = ["Arizona Cardinals", "Atlanta Falcons", "Carolina Panthers", "Dallas Cowboys", "Chicago Bears", "Detroit Lions", 
-        "Green Bay Packers", "New York Giants", "Los Angeles Rams", "Minnesota Vikings", "New Orleans Saints",
-         "Philadelphia Eagles", "San Francisco 49ers", "Seattle Seahawks", "Tampa Bay Buccaneers", "Washington Football Team"]
+class Player(Game):
+    def get_team(self):
+        teams = dict()
+        random.shuffle(self.name)
+        teams['NFC'] = self.name[0]
+        teams['AFC'] = self.name[1]
+        return teams
 
-        for name in random.sample(nfc_team, 1):
-            nfc_team = name
-            nfc_team = nfc_team.upper()
-
-        footballGame.nfc_team = nfc_team
-
-    def afcTeamRandom():
-        afc_team = ["Baltimore Ravens", "Buffalo Bills", "Cincinatti Bengals", "Cleveland Browns", "Denver Broncos", "Houston Texans", 
-        "Indianapolis Colts", "Jacksonville Jaguars", "Kansas City Chiefs", "Las Vegas Raiders", "Los Angeles Chargers",
-        "Miami Dolphins", "New England Patriots", "New York Jets", "Pittsburgh Steelers", "Tennessee Titans"]
-
-        for name in random.sample(afc_team, 1):
-            afc_team = name
-            afc_team = afc_team.upper()
-
-        footballGame.afc_team = afc_team
+my_team = Player(players)
+my_team = my_team.get_team()
+for team, name in my_team.items():
+    print(team, name)
