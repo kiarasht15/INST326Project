f5e32fd0e020db54721d3453b8050bc01a804908
Eunbi Kim <89893789+eunbiKim0135@users.noreply.github.com>
2021-12-05 22:27:30 -0500

    Update Football.py

--- Football.py
+++ Football.py
@@ -18,3 +18,28 @@ my_team = Contestant(contestants)
 my_team = my_team.get_team()
 for team, name in my_team.items():
     print(team, name) 
+   
+""" Make a class and instance of that class; Put the team’s player roster into the text file and retrieve that text. Make sure the file is readable."""
+
+class Roster:
+   """Put the team's player roster into the text file and retrieve that text
+    Attributes:
+        roster (str) : the team's player roster
+    """
+    def __init__(self, file):
+        """Initialize attribute for creating file and read the file
+        Args:
+            roster (list) : the team's player roster
+        """
+        self.roster = list()
+        
+        with open(file, "w", encoding="utf-8")
+    
+    
+    def findRoster(self, players):
+        """Retrieve the text file of the team's player roster
+        Args:
+            players (text) : a text file of the teams' player roster
+        """
+        with open(players, "r", encoding="utf-8")
+            
