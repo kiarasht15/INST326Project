8b35ec2647239b0b08cc8c6e40a26a0b1b0263b3
kiarasht15 <90223700+kiarasht15@users.noreply.github.com>
2021-12-06 11:56:37 -0500

    Update FootballTeams.py

--- FootballTeams.py
+++ FootballTeams.py
@@ -1,20 +1,22 @@
 import random
-teams = ["Arizona Cardinals", "Atlanta Falcons", "Carolina Panthers", "Dallas Cowboys", "Chicago Bears", "Detroit Lions", "Green Bay Packers", "New York Giants", "Los Angeles Rams", "Minnesota Vikings", "New Orleans Saints", "Philadelphia Eagles", "San Francisco 49ers", "Seattle Seahawks", "Tampa Bay Buccaneers", "Washington Football Team"]
-players = ["Baltimore Ravens", "Buffalo Bills", "Cincinatti Bengals", "Cleveland Browns", "Denver Broncos", "Houston Texans", "Indianapolis Colts", "Jacksonville Jaguars", "Kansas City Chiefs", "Las Vegas Raiders", "Los Angeles Chargers", "Miami Dolphins", "New England Patriots", "New York Jets", "Pittsburgh Steelers", "Tennessee Titans"]
+teams = ["Arizona Cardinals", "Atlanta Falcons", "Carolina Panthers", "Dallas Cowboys",
+         "Chicago Bears", "Detroit Lions", "Green Bay Packers", "New York Giants",
+         "Los Angeles Rams", "Minnesota Vikings", "New Orleans Saints", "Philadelphia Eagles",
+         "San Francisco 49ers", "Seattle Seahawks", "Tampa Bay Buccaneers", "Washington Football Team"]
+contestants = ["Baltimore Ravens", "Buffalo Bills", "Cincinatti Bengals", "Cleveland Browns",
+               "Denver Broncos", "Houston Texans", "Indianapolis Colts", "Jacksonville Jaguars",
+               "Kansas City Chiefs", "Las Vegas Raiders", "Los Angeles Chargers", "Miami Dolphins",
+               "New England Patriots", "New York Jets", "Pittsburgh Steelers", "Tennessee Titans"]
 
 class Game:
     def __init__(self, name):
         self.name = name
 
-class Player(Game):
-    def get_team(self):
-        teams = dict()
+class Contestant(Game):
+    def retrieve_team(self):
         random.shuffle(self.name)
-        teams['NFC'] = self.name[0]
-        teams['AFC'] = self.name[1]
-        return teams
+        return {'AFC':self.name[0], 'NFC':self.name[1]}
 
-my_team = Player(players)
-my_team = my_team.get_team()
+my_team = Contestant(contestants).retrieve_team()
 for team, name in my_team.items():
     print(team, name)
