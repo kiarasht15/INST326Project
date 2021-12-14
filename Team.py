90a9ceed49dc1e6b80ecf221da1b0e480fcd5a6c
colbychung01 <74882030+colbychung01@users.noreply.github.com>
2021-12-14 12:27:00 -0500

    main file and my section

---
+++ Team.py
@@ -0,0 +1,45 @@
+import sys
+import random
+
+class Team:
+
+    def __init__(self, team_name, combined_running_skill, combined_throwing_skill, skill_token, roster):
+        """Creates an instance of a team object
+        Attributes:
+        team_name (String) : Name of the team
+        combined_running_skill (int) : How good the team is at running the ball
+        combined_throwing_skill (int) : How good the team is at throwing the ball
+        skill_token (int) : How many tokens the team has to increase their skill 
+        roster (Roster) : The players in the team
+
+        """
+        self.team_name = team_name
+        
+        running_skill = random.randint(1,100)
+        throwing_skill = random.randint(1,100)
+
+        self.combined_running_skill = running_skill
+        self.combined_throwing_skill = throwing_skill
+        self.roster = roster
+    
+    def increase_throwing_skill(self):
+      """Uses a skill token to increase the throwing skill for the team.
+      
+      """
+      if self.skil_token > 0 and self.combined_throwing_skill < 100:
+        if self.combined_throwing_skill + 10 > 100:
+          increase = 100 - self.combined_throwing_skill
+          combined_throwing_skill = self.combined_throwing_skill + increase
+        else:
+          combined_throwing_skill = self.combined_throwing_skill + 5        
+    
+    def increase_running_skill(self):
+      """Uses a skill token to increase the running skill for the team.
+      
+      """
+      if self.skil_token > 0 and self.combined_running_skill < 100:
+        if self.combined_running_skill + 10 > 100:
+          increase = 100 - self.combined_running_skill
+          combined_running_skill = self.combined_running_skill + increase
+        else:
+          combined_running_skill = self.combined_running_skill + 5
---
+++ main.py
@@ -0,0 +1,15 @@
+import Team
+import sys
+import random
+
+def increase_skill(team_name):
+    number = input("Enter 1 to increase running skill or enter 2 to increase throwing skill")
+    
+    if number == 1:
+        team_name.increase_running_skill()
+    elif number == 2:
+        team_name.increase_throwing_skill()
+
+
+    
+        
