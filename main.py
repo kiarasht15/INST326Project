708b2c4b6130b2b8f6d806ec940d285abdacb55b
Eunbi Kim <89893789+eunbiKim0135@users.noreply.github.com>
2021-12-20 06:59:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import Team
+import Roster
 import sys
 import random
-From Roster import roster_file, find_roster
+
 
 def increase_skill(team_name):
     number = input("Enter 1 to increase running skill or enter 2 to increase throwing skill")
@@ -11,6 +12,9 @@ def increase_skill(team_name):
     elif number == 2:
         team_name.increase_throwing_skill()
 
-
+def main(player1,player2):
+    player1 = 
+    player2 = 
     
-        
+    
+    
