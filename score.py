0cdb82c950bf14b86ee0612b6f5f6d328ea0f32b
Eunbi Kim <89893789+eunbiKim0135@users.noreply.github.com>
2021-12-21 19:42:51 -0500

    Update score.py

--- score.py
+++ score.py
@@ -1,23 +1,25 @@
+
+
+
+
 def even():
+    even = random.radint(1,3)
+    return even
+def odd():
+    odd = random.radint(1,3)
+    return odd
+
+def cal_score(even, odd):
+    touchdown = 0
+    if even > 1:
+        touchdown += 3
+        result = even + odd + touchdown
+        return result
+    if odd > 1:
+        touchdown += 0 
+         result = even + odd + touchdown
+        return result
     
 
-
-
-
-
-
-
-
-
-
-
-
-
-def cal_score():
-    even = 
-    odd =
-    touchdown =  
-    
-    
     
     
