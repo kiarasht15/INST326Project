1244a250655af776fbbf65671fcf2867bf7edb1f
kiarasht15 <90223700+kiarasht15@users.noreply.github.com>
2021-12-21 17:39:52 -0800

    Update roster.py

--- roster.py
+++ roster.py
@@ -95,7 +95,7 @@ def find_roster(chosen_name, team_roster):
         Raises:
             ValueError : if chosen team name is not in the team roster
         """
-            if chosen_name in team_names:
-                return team_roster[chosen_name]
-            if not chosen_name in team_roster:
-                raise ValueError(" ")
+        if chosen_name in team_names:
+            return team_roster[chosen_name]
+        if not chosen_name in team_roster:
+            raise ValueError(" ")
