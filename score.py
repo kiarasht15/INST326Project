bedc91a4d547db6f54e2bd4ede6f03cf0c51b4d8
colbychung01 <74882030+colbychung01@users.noreply.github.com>
2021-12-21 22:37:19 -0500

    Update score.py

--- score.py
+++ score.py
@@ -3,15 +3,34 @@ import random
 
 
 def even():
-    """random choose from 1 to 3 for even"""
-    even = random.randint(1,3)
-    return even
+    """random choose from 1 to 10 for even"""
+    while check is True
+        check = false
+        even = random.randint(1,10)
+        if even % 2 == 0:
+            return even
+        else:
+            check = false
 def odd():
-    """random choose from 1 to 3 for odd"""
-    odd = random.randint(1,3)
-    return odd
+    """random choose from 1 to 11 for odd"""
+    while check is True
+        check = false
+        odd = random.randint(1,11)
+        if odd % 2 != 0:
+            return odd
+        else:
+            check = false
 
-def cal_score(even, odd):
+def compare():
+    even = even()
+    odd = odd()
+    
+    if even > odd:
+        return 8
+    elif even < odd:
+        return 3
+
+def cal_score():
     """calculate the total score 
     Args:
         even (int) : even score by randomly choosing 1 to 3
@@ -19,16 +38,18 @@ def cal_score(even, odd):
     Returns:
         (int) : the result by calculating even, odd, and touchdown
     """
-    touchdown = 0
-    if even > 1:
-        touchdown += 3
-        result = even + odd + touchdown
-        return result
-    if odd > 1:
-        touchdown += 0 
-        result = even + odd + touchdown
-        return result
-
+    touchdown_team = 0
+    counter = 0
+    
+    While counter < 5:
+        score_team = compare()
+        
+        touchdown_team = touchdown_team + score_team
+        
+        counter = counter + 1
+        
+    
+    return touchdown_team
     
 
     
